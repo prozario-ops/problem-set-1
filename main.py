f82f8dcc107b9fb175ed53ecb38e5e0db38ab12b
prozario-ops <prozario@umd.edu>
2026-02-23 20:23:39 -0500

    Format main.py: Clean up whitespace and improve readability

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+
 '''
 You will run this problem set from main.py so set things up accordingly
 '''
@@ -8,10 +10,10 @@ import src.transform_load
 # Call functions / instanciate objects from the two analysis .py files
 def main():
         # Call functions from extract.py
-
+        weather_df= src.extract.extract_weather_data()
 
         # Call functions from transform_load.py
-
+        transit_df= src.transform_load.transform_transit_data()
 
 
 if __name__ == "__main__":
